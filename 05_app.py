20e57d82cc7a73c2eea0d04d98a0381eed1c5398
Michaelleonb <i202519146@cibertec.edu.pe>
2025-09-23 03:57:40 +0000

    ejercicios en clase

---
+++ 05_app.py
@@ -0,0 +1,13 @@
+# Obtener el promedio de un estudiante a partir de 3 notas
+
+# Inicio: Definir variables de las notas
+nota_t1 = 16
+nota_t2 = 10
+nota_ef = 13
+# Proceso: Definir el proceso de cálculo del promedio de notas
+# t1(20%) + t2 (35%) + ef (45%)
+promedio_de_nota=(nota_t1*.2+nota_t2*.35+nota_ef*.45)
+
+# Salida: Mostrar el resultado del promedio
+print(f"el promedio de nota es: {promedio_de_nota}")
+
