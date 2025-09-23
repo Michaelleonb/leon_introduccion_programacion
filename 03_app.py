d5824748dab517dbebcecd82148b64b31860185d
Michaelleonb <i202519146@cibertec.edu.pe>
2025-09-23 03:01:46 +0000

    ejercicios de programación con python

--- 03_app.py
+++ 03_app.py
@@ -11,3 +11,32 @@ name = "Alberto"
 print(nombre)
 print(telefono)
 print(name)
+
+# Jugando con variables
+age = 30
+print(age)
+print(f"Edad: {age}")
+
+district = "San Luis"
+print(district)
+print(f"Nombre de Distrito: {district}")
+
+# Precio de un producto
+price = 45.99
+print(price)
+print(f"El precio es: {price}")
+
+# Es un cliente activo
+is_active_client = True
+print(is_active_client)
+print(f"¿Está activo? {is_active_client}")
+
+# trabajando con fechas 
+from datetime import date
+fecha_clase = date(1992,7,1) # (yyyy, mm, dd)
+print(fecha_clase)
+print(f"La fecha de hoy es: {fecha_clase}")
+
+fecha_formateada = fecha_clase.strftime("%d/%m/%Y")
+print(f"La fecha con formato es: {fecha_formateada}")
+
