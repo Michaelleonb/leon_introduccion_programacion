037d049cf2b38ec49de3f301b94807b07794073a
Michaelleonb <i202519146@cibertec.edu.pe>
2025-09-30 02:53:36 +0000

    clase del 29 de septiembre

---
+++ 16_app.py
@@ -0,0 +1,19 @@
+# Estructura de bucle For
+
+# Sintaxis
+# for value in sequence:
+#   instrucción, que es lo que debe hacer mientras se cumple el bucle
+
+# Ejemplo:
+
+# Listado de frutas
+frutas = ["pera", "platano", "uva", "arandanos", "aguaymanto"]
+
+for fruta in frutas:
+    print(fruta)
+
+    # Listado de paises
+paises = ["peru", "paraguay", "chile", "colombia", "mexico", "venezuela"]
+
+for pais in paises:
+    print(pais)
